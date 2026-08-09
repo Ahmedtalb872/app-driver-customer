a7f47e6bf80301f05f7f9c62c85a7363945b3ceb
Ahmedtalb872 <Ahmedtalb872@users.noreply.github.com>
2026-08-09 22:04:26 +0000

    deploy: 69716dea8441fb1002ff43fb2c4876e84e17ffde

--- customer-app/main.dart.js
+++ customer-app/main.dart.js
@@ -39818,7 +39818,7 @@ $S:2}
 A.beU.prototype={
 $1(a){var s=A.f5().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5a2a6a42cce67f965cf540fcecf616faca624aa1/":s)+a},
 $S:41}
 A.a2x.prototype={
 gv(a){var s=this.a
